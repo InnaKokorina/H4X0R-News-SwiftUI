e253815d044b104bd79aec6e887419fe9fabef8d
InnaKokorina <93093046+InnaKokorina@users.noreply.github.com>
2022-03-04 14:46:29 +0400

    add detailView for looking at the news in detail

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -13,12 +13,13 @@ struct ContentView: View {
     var body: some View {
         NavigationView{
             List(networkManager.posts) { post in
-                HStack {
-                    Text(String(post.points))
-                    Text(post.title)
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
                 }
-
-                     }
+            }
             .navigationTitle("H4X0R NEWS")
             
         }
---
+++ Views/DetailView.swift
@@ -0,0 +1,43 @@
+//
+//  DetailView.swift
+//  H4X0R News SwiftUI
+//
+//  Created by Inna Kokorina on 04.03.2022.
+//
+
+import SwiftUI
+import WebKit
+
+
+struct DetailView: View {
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com")
+    }
+}
+
+struct WebView: UIViewRepresentable {
+
+    
+    let urlString:String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString){
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+    
+}
